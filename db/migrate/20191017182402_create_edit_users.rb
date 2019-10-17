19f688c1e4f7364fb42d7688e941e1f2fedf8c33
GrapeColor <grapecolor@GrapeColor.local>
2019-10-18 03:36:02 +0900

    edit edit_user

--- db/migrate/20191017182402_create_edit_users.rb
+++ db/migrate/20191017182402_create_edit_users.rb
@@ -1,6 +1,8 @@
 class CreateEditUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :edit_users do |t|
+      t.integer :book_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
