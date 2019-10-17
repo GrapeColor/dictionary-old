80551e419d91112d2f15d231590428b56084c51d
GrapeColor <grapecolor@GrapeColor.local>
2019-10-18 03:34:37 +0900

    edit book

--- db/migrate/20191017182345_create_books.rb
+++ db/migrate/20191017182345_create_books.rb
@@ -1,8 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.boolean :edit, null: false, default: false
 
       t.timestamps
     end
+    add_index :books, :title
   end
 end
